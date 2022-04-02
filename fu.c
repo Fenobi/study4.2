57bf68c31b05c6551f045c0e98521db3f2444ad2
fu-qingyun <1366773912@qq.com>
2022-04-02 12:11:20 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//#include <math.h>
+//int main()
+//{
+//	printf("请输入自幂数的位数:>");
+//	int x = 0;
+//	scanf("%d", &x);
+//	x = pow(10, x);
+//	int i = 0;
+//	for (i = 0; i <= x; i++)
+//	{
+//		int n = 0;
+//		int tmp1 = i;
+//		while (tmp1)//确定位数n
+//		{
+//			tmp1 /= 10;
+//			n++;
+//		}
+//		int tmp2 = i;
+//		int sum = 0;
+//		while (tmp2)//计算
+//		{
+//			sum += pow(tmp2 % 10, n);
+//			tmp2 /= 10;
+//		}
+//		if (sum == i)//判断
+//		{
+//			printf("自幂数=%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
